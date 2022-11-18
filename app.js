ed2621496b54804656c936ba80f2b56db435be0c
mikia <mikiakuta0529@gmail.com>
2022-11-18 03:06:06 +0000

    Use passport to add Cookies and Sessions. Implement Google Oauth

--- app.js
+++ app.js
@@ -2,9 +2,48 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-// const md5 = require('md5');
-const bcrypt = require('bcrypt');
-const saltRounds = 12;
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const User = require('./models/users')
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(session({
+    secret: 'Thisisourlittlesecret',
+    resave: false,
+    saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 main().catch(err => console.log(err));
 
@@ -13,70 +52,103 @@ async function main() {
     console.log('MONGO connected')
 }
 
-const User = require('./models/users')
-
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile'] })
+)
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.redirect('/secrets');
+    });
+
+app.get('/login', (req, res) => {
+    res.render('login')
 })
 
 app.get('/register', (req, res) => {
     res.render('register')
 })
 
-app.post('/register', (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        const user = new User({
-            email: req.body.username,
-            password: hash
-        })
-        user.save((err) => {
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated()) {
+        User.find({ 'secret': { $ne: null } }, (err, foundUsers) => {
             if (err) {
                 console.log(err)
             } else {
-                res.render('secrets')
+                if (foundUsers) {
+                    res.render('secrets', { foundUsers })
+                }
             }
         })
-    })
+    } else {
+        res.redirect('/login')
+    }
 })
 
-app.get('/login', (req, res) => {
-    res.render('login')
+app.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            console.log(err)
+        }
+    });
+    res.redirect('/')
 })
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    User.findOne({ email: username }, function (err, foundUser) {
+app.get('/submit', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('submit')
+    } else {
+        res.redirect('/login')
+    }
+})
+
+app.post('/register', (req, res) => {
+    User.register({ username: req.body.username }, req.body.password, (err, user) => {
         if (err) {
             console.log(err)
         } else {
-            if (foundUser) {
-                bcrypt.compare(password, foundUser.password, function (error, result) {
-                    if (result === true) {
-                        res.render('secrets')
-                    }
-                })
-            }
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets')
+            })
         }
     })
 })
 
-// app.post('/login', async (req, res) => {
-//     const { username, password } = req.body;
-//     const foundUser = await User.findOne({ email: username });
-//     bcrypt.compare(password, foundUser.password)
-//         .then((result) => {
-//             console.log(result)
-//         })
-//         .catch((error) => {
-//             console.log(error)
-//         });
-//     res.render('secrets')
+app.post('/login', (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            passport.authenticate('local')(req, res, () => {
+                res.redirect('/secrets')
+            })
+        }
+    })
+})
 
-// })
+app.post('/submit', (req, res) => {
+    const { secret } = req.body;
+    User.findById(req.user.id, (err, foundUser) => {
+        if (err) {
+            console.log(err)
+        } else {
+            foundUser.secret = secret;
+            foundUser.save(() => {
+                res.redirect('/secrets')
+            })
+        }
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('LISTENING ON PORT 3000')
--- models/users.js
+++ models/users.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
 
 const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+    username: String,
+    password: String,
+    googleId: String,
+    secret: String
 })
 
 // const secret = process.env.SECRET;
-// userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 module.exports = mongoose.model('User', userSchema);
